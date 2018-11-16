9e9f44b9fe904c2cbf22a789cb96da19c1bbbd77
Majid Alavizadeh <majidonline@gmail.com>
2018-11-16 12:40:47 +0330

    Food words translation

---
+++ resources/lang/en/foods.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: majid
+ * Date: 11/16/18
+ * Time: 11:18 AM
+ */
+
+return [
+    'not_exists' => 'The :attribute food does not exists.',
+    'not_parameters' => 'The parameters sent are incomplete',
+    'dont_eat_it' => 'Don’t Eat it!',
+    'eat_it' => 'Eat up its safe!',
+
+];
