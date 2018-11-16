7657fad7a1b0465e86fff21bc4588bf1c8320178
Majid Alavizadeh <majidonline@gmail.com>
2018-11-16 12:41:11 +0330

    Create Food interface

---
+++ app/Interfaces/FoodResponse.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: majid
+ * Date: 11/16/18
+ * Time: 11:37 AM
+ */
+
+namespace App\Interfaces;
+
+
+interface FoodResponse
+{
+    public function getResponse();
+}
